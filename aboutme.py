e53b08498cdce6e579f425d3a221fe9f0229f2c3
imakki <ankit.escalate360@gmail.com>
2018-03-07 16:04:43 +0530

    first 2 python programs

---
+++ aboutme.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+print ('Hello!, I can code in python');
+
+print('Here\'s a picture of a dog:');
+print('O-####-');
+print('  ||||');
+
+print('I live in a glassglow')
+print('   __|__');  
+print('   |o o|');
+print('   |o o|/---\_');
+print('   |o o|    |');
+print('   |o o| ( )|');
+print('   ==========');
